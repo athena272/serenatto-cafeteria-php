25555121fe70f63ee98b5b0ec56fc9ade3fcf807
athena272 <guilhermera272@gmail.com>
2025-06-10 14:17:54 -0300

    Update Product.php

--- src/Domain/Model/Product.php
+++ src/Domain/Model/Product.php
@@ -22,4 +22,28 @@ class Product
         $this->image = $image;
         $this->price = $price;
     }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getType(): string {
+        return $this->type;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    public function getImage(): string {
+        return $this->image;
+    }
+
+    public function getPrice(): float {
+        return $this->price;
+    }
 }
