b9e0033a0061f2250f12dccbeb2f8d8a03f449b6
athena272 <guilhermera272@gmail.com>
2025-06-10 21:18:00 -0300

    Create insert-schema.sql

---
+++ src/Schemas/insert-schema.sql
@@ -0,0 +1,28 @@
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Coffee', 'Creamy Coffee', 'Irresistibly smooth creamy coffee that delights your palate', 'cafe-cremoso.jpg',
+        '5.00');
+
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Coffee', 'Coffee with Milk', 'The perfect harmony of coffee and milk, a comforting experience',
+        'cafe-com-leite.jpg', '2.00');
+
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Coffee', 'Cappuccino', 'Smooth coffee, creamy milk, and a touch of sweet flavor', 'cappuccino.jpg', '7.00');
+
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Coffee', 'Iced Coffee', 'Refreshing iced coffee, sweetened and with subtle hints of vanilla or caramel.',
+        'cafe-gelado.jpg', '3.00');
+
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Lunch', 'Steak', 'Steak, rice with beans, and delicious French fries', 'bife.jpg', '27.90');
+
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Lunch', 'Fish Fillet', 'Baked salmon fillet, rice, green beans, and tomato.', 'prato-peixe.jpg', '24.99');
+
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Lunch', 'Chicken', 'Delicious breaded chicken with French fries, cabbage salad, and spicy sauce',
+        'prato-frango.jpg', '23.00');
+
+INSERT INTO `serenatto`.`products` (`type`, `name`, `description`, `image`, `price`)
+VALUES ('Lunch', 'Fettuccine', 'Authentic Italian fettuccine pasta with grilled chicken breast', 'fettuccine.jpg',
+        '22.50');
