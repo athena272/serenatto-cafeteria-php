aed8b94759c3dcf368e35218435eed7bd5355520
rosario <58920070+athena272@users.noreply.github.com>
2025-10-21 14:33:26 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 var preco = $('#preco').maskMoney()
+var price = $('#price').maskMoney({
+    prefix: 'R$ ',
+    allowNegative: false,
+    thousands: '.',
+    decimal: ',',
+    affixesStay: true
+});
