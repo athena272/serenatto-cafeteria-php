79c7527f2fcfa6bd7730d61ffaf80606ba2cf919
rosario <58920070+athena272@users.noreply.github.com>
2025-10-21 14:33:44 -0300

    Update create-product.php

--- create-product.php
+++ create-product.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 require_once 'vendor/autoload.php';
 
@@ -43,11 +44,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['flash_message'] = "Error creating product: " . $e->getMessage();
     }
 }
-
 ?>
 
 <!doctype html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -57,55 +57,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/admin.css">
     <link rel="stylesheet" href="css/form.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="icon" href="img/icone-serenatto.png" type="image/x-icon">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap"
-          rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <title>Serenatto - Cadastrar Produto</title>
+    <title>Serenatto - Add Product</title>
 </head>
 <body>
 <main>
     <section class="container-admin-banner">
-        <img src="img/logo-serenatto-horizontal.png" class="logo-admin" alt="logo-serenatto">
-        <h1>Cadastro de Produtos</h1>
+        <img src="img/logo-serenatto-horizontal.png" class="logo-admin" alt="serenatto-logo">
+        <h1>Add Product</h1>
         <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
     </section>
-    <section class="container-form">
-        <form method="post">
 
-            <label for="nome">Nome</label>
-            <input type="text" id="nome" name="nome" placeholder="Digite o nome do produto" required>
+    <?php if (isset($_SESSION['flash_message'])): ?>
+        <div class="flash-message">
+            <?= htmlspecialchars($_SESSION['flash_message']) ?>
+        </div>
+        <?php unset($_SESSION['flash_message']); ?>
+    <?php endif; ?>
+
+    <section class="container-form">
+        <form method="post" enctype="multipart/form-data">
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Enter product name" required>
+
             <div class="container-radio">
                 <div>
-                    <label for="cafe">Café</label>
-                    <input type="radio" id="cafe" name="tipo" value="Café" checked>
+                    <label for="coffee">Coffee</label>
+                    <input type="radio" id="coffee" name="type" value="Coffee" checked>
                 </div>
                 <div>
-                    <label for="almoco">Almoço</label>
-                    <input type="radio" id="almoco" name="tipo" value="Almoço">
+                    <label for="lunch">Lunch</label>
+                    <input type="radio" id="lunch" name="type" value="Lunch">
                 </div>
             </div>
-            <label for="descricao">Descrição</label>
-            <input type="text" id="descricao" name="descricao" placeholder="Digite uma descrição" required>
 
-            <label for="preco">Preço</label>
-            <input type="text" id="preco" name="preco" placeholder="Digite uma descrição" required>
+            <label for="description">Description</label>
+            <input type="text" id="description" name="description" placeholder="Enter a description" required>
 
-            <label for="imagem">Envie uma imagem do produto</label>
-            <input type="file" name="imagem" accept="image/*" id="imagem" placeholder="Envie uma imagem">
+            <label for="price">Price</label>
+            <input type="text" id="price" name="price" placeholder="Enter the price" required>
 
-            <input type="submit" name="cadastro" class="botao-cadastrar" value="Cadastrar produto"/>
+            <label for="image">Product Image</label>
+            <input type="file" name="image" accept="image/*" id="image">
+
+            <input type="submit" class="botao-cadastrar" value="Create Product"/>
         </form>
-
     </section>
 </main>
 
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"
-        integrity="sha512-Rdk63VC+1UYzGSgd3u2iadi0joUrcwX0IWp2rTh6KXFoAmgOjRS99Vynz1lJPT8dLjvo6JZOqpAHJyfCEZ5KoA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js" integrity="sha512-Rdk63VC+1UYzGSgd3u2iadi0joUrcwX0IWp2rTh6KXFoAmgOjRS99Vynz1lJPT8dLjvo6JZOqpAHJyfCEZ5KoA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="js/index.js"></script>
 </body>
 </html>
